978bcd0eeeeeb10ac12c83fc3bbb96ab9d0e70e2
Salma Belassiri <salmabelassiri@gmail.com>
2021-02-15 22:37:39 +0100

    add project

---
+++ gestion des etudiants/gestion des etudiants/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using gestion_des_etudiants.Models;
+
+namespace gestion_des_etudiants.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<gestion_des_etudiants.Models.student> student { get; set; }
+    }
+}
---
+++ gestion des etudiants/gestion des etudiants/Data/Migrations/20210215212858_FirstSetUp.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace gestion_des_etudiants.Data.Migrations
+{
+    public partial class FirstSetUp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(nullable: true),
+                    Prenom = table.Column<string>(nullable: true),
+                    CIN = table.Column<string>(nullable: true),
+                    Adresse = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_student", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "student");
+        }
+    }
+}
