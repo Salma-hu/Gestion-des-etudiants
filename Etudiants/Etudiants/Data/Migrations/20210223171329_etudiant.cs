2a518536d3c06dc530ba4362e3ae813a0f06809c
Salma Belassiri <salmabelassiri@gmail.com>
2021-02-24 09:45:04 +0100

    add project

---
+++ Etudiants/Etudiants/Data/Migrations/20210223171329_etudiant.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Etudiants.Data.Migrations
+{
+    public partial class etudiant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Etudiant",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(nullable: true),
+                    Prenom = table.Column<string>(nullable: true),
+                    CIN = table.Column<string>(nullable: true),
+                    Adresse = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Etudiant", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Etudiant");
+        }
+    }
+}
